6aaf76bba34d995231dad00abc37589f55ff9389
huanghaifeng1 <huanghaifeng1@jd.com>
2018-05-25 17:44:04 +0800

    6. conversion

---
+++ conversion.rs
@@ -0,0 +1,40 @@
+use std::convert::From;
+use std::string::ToString;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+struct Circle {
+    radius: i32
+}
+
+impl ToString for Circle {
+    fn to_string(&self) -> String {
+        format!("Circle of radius {:?}", self.radius)
+    }
+}
+
+fn main() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+
+    let circle = Circle { radius: 6 };
+    println!("{}", circle.to_string());
+
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+    let sum = parsed + turbo_parsed;
+    println!("Sum: {:?}", sum);
+}
